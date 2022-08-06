5139e05a9e9d9ac85b40e1a5c194d95408b6a46f
Gisle Aas <gisle.aas@uib.no>
2022-08-06 16:16:36 +0200

    Cache data fetched from tk-topdesk API

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,5 +1,17 @@
+let services = null;
+let services_fetch_time = null;
+
 export async function fetch_services() {
+    if (services) {
+        const age = Date.now() - services_fetch_time;
+        console.log(`Returning services from cache with age=${age/1000}s`)
+        return services;
+    }
+    const before = Date.now();
     const res = await fetch('https://gw-uib.intark.uh-it.no/tk-topdesk/svc.json?app=nexttk');
-    const services = await res.json();
+    services = await res.json();
+    const after = Date.now();
+    console.log(`Fetched data from tk-topdesk API in ${after - before}ms`)
+    services_fetch_time = after;
     return services;
 }
