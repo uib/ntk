5c37d213bc539d0f346bcf351ab0ec4d73cdadc2
Gisle Aas <gisle.aas@uib.no>
2022-08-07 13:20:41 +0200

    Include number of services

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({services}) {
       </Head>
 
       <main className={styles.main}>
-        <h1>UiBs tjenesteportefølje</h1>
+        <h1>UiBs tjenesteportefølje ({services.length})</h1>
         <ul className={styles.serviceList}>
         { services.map(svc => <li key={svc.id} className={styles['foo-bar']}><Link href={'/' + svc.id}><a className={styles['lifecycle-' + svc.lifecycle.substr(0, 1)]} title={svc.name != svc.short_name && svc.name}><span className={styles.serviceId}>{svc.id}</span> {svc.short_name}</a></Link></li>) }
         </ul>
