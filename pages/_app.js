5cb3fbabab84e4ab9e558a26ede8e02bdb905b13
Gisle Aas <gisle.aas@uib.no>
2022-08-12 16:24:01 +0200

    Follow advice on https://fontawesome.com/v5/docs/web/use-with/react#getting-font-awesome-css-to-work to make icons styled properly

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import '../styles/globals.css';
+
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 import Layout from '../components/layout';
 
 export default function MyApp({ Component, pageProps }) {
