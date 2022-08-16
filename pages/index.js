d6f7a591827b7473b7d98405a844420e8c43f9d2
Gisle Aas <gisle.aas@uib.no>
2022-08-16 16:08:42 +0200

    Legg til når data ble hentet fra Topdesk

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import { fetch_services } from '../utils/fetch'
 
-export default function Home({services}) {
+export default function Home({services, synced_date}) {
   return (
     <>
       <Head>
@@ -24,6 +24,7 @@ export default function Home({services}) {
             </li>)
         }
         </ul>
+        <p><em>Data hentet fra UiBhjelp {synced_date}Z</em></p>
       </div>
     </>
   )
@@ -32,6 +33,9 @@ export default function Home({services}) {
 export async function getStaticProps(context) {
   const services = await fetch_services();
   return {
-    props: { services: services.map(({id, short_name, name, lifecycle}) => ({id, short_name, name, lifecycle})) }
+    props: {
+      services: services.map(({id, short_name, name, lifecycle}) => ({id, short_name, name, lifecycle})),
+      synced_date: services.map(svc => svc.meta.sdate).reduce((p,c) => p < c ? p : c, 'na').substr(0, 16).replace('T', ' '),
+    }
   }
 }
