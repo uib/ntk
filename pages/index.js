1f57e82ae21e5e68b8c1ccc643f4c8280a44475a
Gisle Aas <gisle.aas@uib.no>
2022-09-17 09:04:34 +0200

    First stab at filtering

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,14 @@ import Link from 'next/link'
 import { ServiceLink } from '../components/servicelink'
 import styles from '../styles/Home.module.css'
 import { fetch_services } from '../utils/fetch'
+import { useState } from 'react'
 
 export default function Home({services, synced_date}) {
+  const [filter, setFilter] = useState('');
+  const filter_lc = filter.trim().toLowerCase();
+  const filtered_services = services.filter(svc =>
+    !filter_lc || svc.search_body.includes(filter_lc)
+  );
   return (
     <>
       <Head>
@@ -13,9 +19,13 @@ export default function Home({services, synced_date}) {
       </Head>
 
       <div className={styles.main}>
-        <h1>UiBs tjeneste&shy;portefølje ({services.length})</h1>
+        <input
+            value={filter}
+            onChange={event => setFilter(event.target.value)}
+        />
+        <h1>UiBs tjeneste&shy;portefølje ({filtered_services.length}{filter_lc ? ` med "${filter}"` : undefined})</h1>
         <ul className={styles.serviceList}>
-        { services.map(svc =>
+        { filtered_services.map(svc =>
             <li key={svc.id}>
               <ServiceLink service={svc} />
             </li>)
@@ -31,7 +41,20 @@ export async function getStaticProps(context) {
   const services = await fetch_services();
   return {
     props: {
-      services: services.map(({id, short_name, name, lifecycle}) => ({id, short_name, name, lifecycle})),
+      services: services.map(svc => (
+        {
+          id: svc.id,
+          short_name: svc.short_name,
+          name: svc.name,
+          lifecycle: svc.lifecycle,
+          search_body: [
+            svc.id,
+            svc.asset_name,
+            svc.name,
+            svc.description
+          ].join('\n').toLowerCase(),
+        }
+      )),
       synced_date: services.map(svc => svc.meta.sdate).reduce((p,c) => p < c ? p : c, 'na').substr(0, 16).replace('T', ' '),
     }
   }
