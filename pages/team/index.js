06aae901ba8cbd23b69343742a12cc4553575e24
Gisle Aas <gisle.aas@uib.no>
2023-02-13 15:29:34 +0100

    Set up teams page as a table

--- pages/team/index.js
+++ pages/team/index.js
@@ -1,14 +1,24 @@
 import { services_by_team, team_slug } from '../../utils/fetch';
 import Link from 'next/link';
+import styles from '../../styles/Team.module.css';
 
 export default function Teams({teams}) {
     return (
-        <div>
-            <h1>Forvaltingsteam med tjenester</h1>
-            <ul>
-                {teams.map(t => <li key={t.slug}><Link href={"/team/" + t.slug}>{t.name} ({t.count})</Link></li>)}
-            </ul>
-        </div>
+        <article>
+            <h1>Forvaltningsteam med tjenester</h1>
+            <p>Vi bruker operatørgrupper i UiBhjelp til å peke ut de som har ansvar for å
+                forvalte tjenestene. Lenkene under går til sider som lister opp tjeneste som hører
+                under hvert enkelt team.
+            </p>
+            <table className={styles.teamTable}>
+                <thead>
+                <tr><th>Team</th><th>Antall tjenester</th></tr>
+                </thead>
+                <tbody>
+{teams.map(t => <tr key={t.slug}><td><Link href={"/team/" + t.slug}>{t.name}</Link></td><td>{t.count}</td></tr>)}
+                </tbody>
+            </table>
+        </article>
     );
 }
 
