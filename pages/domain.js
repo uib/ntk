0c8df8fd15b3c72091ee72e2b20069f0d813cfbb
Gisle Aas <gisle.aas@uib.no>
2023-11-21 12:23:43 +0100

    Classify ssh-scheme URLs correctly
    A small beuty-glitch is that we also accept sshs:-URLs, but I think
    it's better to accept that then to complicate the expression.

--- pages/domain.js
+++ pages/domain.js
@@ -55,7 +55,7 @@ export async function getStaticProps(context) {
     if (svc.id == "TJ0303") continue;
     for (const ref of svc.refs) {
       if (ref.type == "Produksjonsmiljø") {
-        let schemeless = ref.href.replace(/^(ldap|http)s?:\/\//, "");
+        let schemeless = ref.href.replace(/^(ssh|ldap|http)s?:\/\//, "");
         let [host, path] = schemeless.split('/', 2);
         let host_lst = host.split('.');
         let topdomain = [host_lst.pop(), host_lst.pop()].reverse().join('.');
