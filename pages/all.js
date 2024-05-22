ba0fc76e61a995040b435732697a521b037b5e4b
Gisle Aas <gisle.aas@uib.no>
2024-05-22 10:33:28 +0200

    New way to filter for user services

--- pages/all.js
+++ pages/all.js
@@ -32,7 +32,7 @@ export async function getStaticProps(context) {
   return {
     props: {
       services: services
-        .filter(svc => svc.lifecycle.match(/^[23]-/) && svc.servicetype == "Bruker-tjeneste")
+        .filter(svc => svc.lifecycle.match(/^[23] -/) && svc.servicetype == "Brukertjeneste")
         .sort((a, b) => (a.name || a.short_name).toLowerCase().localeCompare((b.name || b.short_name).toLowerCase(), 'nb'))
         .map(({id, short_name, name, description}) => ({id, short_name, name, description}))
     }
