570bec4ba01068c2b98e96cd041efffd1df4b9ce
Gisle Aas <gisle.aas@uib.no>
2025-07-06 12:51:28 +0200

    Include description and improved subtitle

--- pages/[id].js
+++ pages/[id].js
@@ -171,7 +171,39 @@ export async function getStaticProps(context) {
     }
   }
 
+  function wrapText(text, maxWidth) {
+    const words = text.split(' ');
+    const lines = [];
+    let currentLine = '';
+
+    for (const word of words) {
+      if ((currentLine + word).length <= maxWidth) {
+        currentLine += (currentLine ? ' ' : '') + word;
+      } else {
+        lines.push(currentLine);
+        currentLine = word;
+      }
+    }
+    if (currentLine) lines.push(currentLine);
+
+    return lines;
+  }
+
   function generate_badge(svc) {
+    // Information to include
+    const title = svc.short_name
+    let subtitle = svc.name
+    if (subtitle.startsWith(title)) {
+      subtitle = subtitle.slice(title.length).trim();
+      if (subtitle.startsWith(":") || subtitle.startsWith("-") || subtitle.startsWith("–")) {
+        subtitle = subtitle.slice(1).trimStart();
+      }
+    }
+    let desc_lines = wrapText(svc.description ?? '', 90);
+    if (desc_lines.length > 3) {
+      desc_lines[2] += " ...";
+    }
+
     const svcDir = path.join(process.cwd(), 'public', svc.id);
     if (!fs.existsSync(svcDir)) {
       fs.mkdirSync(svcDir);
@@ -185,19 +217,19 @@ export async function getStaticProps(context) {
     </text>
 
     <text x="30" y="80" font-family="sans-serif" font-size="36" fill="#C05A1C" font-weight="bold">
-        ${svc.short_name}
+        ${title}
     </text>
 
     <text x="30" y="110" font-family="sans-serif" font-size="24" fill="#404040">
-        ${svc.name}
+        ${subtitle}
     </text>
 
     <line x1="20" y1="130" x2="774" y2="130" stroke="#C05A1C" stroke-width="3"/>
 
     <text x="30" y="135" font-family="sans-serif" font-size="18" fill="#404040">
-        <tspan x="30" dy="1.2em"></tspan>
-        <tspan x="30" dy="1.2em"></tspan>
-        <tspan x="30" dy="1.2em"></tspan>
+        <tspan x="30" dy="1.3em">${desc_lines[0] ?? ''}</tspan>
+        <tspan x="30" dy="1.3em">${desc_lines[1] ?? ''}</tspan>
+        <tspan x="30" dy="1.3em">${desc_lines[2] ?? ''}</tspan>
     </text>
 </svg>`);
   }
