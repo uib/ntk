9e37d175611c3dcfb313c3b024969a53bef257de
Gisle Aas <gisle.aas@uib.no>
2025-07-28 08:44:53 +0200

    feat(nav): highlight active page in navbar

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,18 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Navbar() {
+    const router = useRouter();
     return (
         <nav>
             <ul>
                 <li><Link href="/"><img src="/logo.svg" alt="Hjem" className="logo" /></Link></li>
-                <li><Link href="/">Porte&shy;føljen</Link></li>
-                <li><Link href="/all">Bruker&shy;tjenester</Link></li>
-                <li><Link href="/domain">Domene&shy;navn</Link></li>
-                <li><Link href="/team">Forvaltere</Link></li>
+                <li className={router.pathname == "/" ? "active" : ""}><Link href="/">Porte&shy;føljen</Link></li>
+                <li className={router.pathname == "/all" ? "active" : ""}><Link href="/all">Bruker&shy;tjenester</Link></li>
+                <li className={router.pathname == "/domain" ? "active" : ""}><Link href="/domain">Domene&shy;navn</Link></li>
+                <li className={router.pathname == "/team" ? "active" : ""}><Link href="/team">Forvaltere</Link></li>
                 <li><a href="https://tjenester.app.uib.no">Power BI</a></li>
-                <li><Link href="/om">Om</Link></li>
+                <li className={router.pathname == "/om" ? "active" : ""}><Link href="/om">Om</Link></li>
             </ul>
         </nav>
     );
